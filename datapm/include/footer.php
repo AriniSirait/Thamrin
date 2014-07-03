33ebc954126481363e4146346687ab1df213c7bc
Arini Hasianna Sirait <arinihasianna@yahoo.com>
2014-07-04 00:27:51 +0700

    versi terbaru untuk aktivasi datepicker

--- datapm/include/footer.php
+++ datapm/include/footer.php
@@ -6,30 +6,32 @@
       </div>
    </div>
    <!-- END FOOTER -->
-   <!-- BEGIN JAVASCRIPTS -->    
+   <!-- BEGIN JAVASCRIPTS -->
    <!-- Load javascripts at bottom, this will reduce page load time -->
    <script src="js/jquery-1.8.3.min.js"></script>
-   <!-- IMPORTANT! Load jquery-ui-1.10.1.custom.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
    <script src="assets/jquery-ui/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-   <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+   <script src="assets/bootstrap/js/bootstrap.min.js"></script>   
    <script src="js/jquery.blockui.js"></script>
    <!-- ie8 fixes -->
    <!--[if lt IE 9]>
    <script src="js/excanvas.js"></script>
    <script src="js/respond.js"></script>
-   <![endif]-->
+   <![endif]-->   
+   <script type="text/javascript" src="assets/uniform/jquery.uniform.min.js"></script>
+   <script type="text/javascript" src="assets/data-tables/jquery.dataTables.js"></script>
+   <script type="text/javascript" src="assets/data-tables/DT_bootstrap.js"></script>
    <script type="text/javascript" src="assets/chosen-bootstrap/chosen/chosen.jquery.min.js"></script>
    <script type="text/javascript" src="assets/uniform/jquery.uniform.min.js"></script>
    <script src="js/scripts.js"></script>
    <script src="js/ui-jqueryui.js"></script>
+   <script src="js/scripts.js"></script>
    <script>
       jQuery(document).ready(function() {       
          // initiate layout and plugins
          App.init();
-         UIJQueryUI.init();
+		 UIJQueryUI.init();
       });
    </script>
-   <!-- END JAVASCRIPTS --> 
 </body>
 <!-- END BODY -->
 </html>
